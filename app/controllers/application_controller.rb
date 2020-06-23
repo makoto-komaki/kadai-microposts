50f1876b6647fa447d6cba0e314c6c13e3af8f7f
makoto-komaki <otokam.ikamok1020@gmail.com>
2020-06-23 12:36:58 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,15 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  private
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
